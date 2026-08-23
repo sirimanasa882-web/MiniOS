37c15336e5c6add7936b4f5f49337fca9823c2d9
sirimanasa882-web <sirimanasa882@gmail.com>
2026-08-23 13:41:58 +0000

    Add program 2

--- program2.c
+++ program2.c
@@ -1,40 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
 int main() {
-    int num;
-    pid_t pid;
-    char str[20];
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    pid = fork();
-
-    if (pid < 0) {
-        printf("Fork Failed\n");
-        exit(1);
-    }
-
-    if (pid == 0) {
-        // Child Process
-        sprintf(str, "%d", num);
-
-        execl("./factorial", "factorial", str, NULL);
-
-        // Executes only if exec fails
-        perror("exec failed");
-    }
-    else {
-        // Parent Process
-        wait(NULL);
-
-        printf("\n--- Parent Process ---\n");
-        printf("Square of %d = %d\n", num, num * num);
-    }
-
+    printf("Hello");
     return 0;
 }
